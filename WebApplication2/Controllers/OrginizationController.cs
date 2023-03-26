287521596f993e7e3d9dad3b5f0384cb8c7b2e6f
thenderizer <105055549+thenderizer@users.noreply.github.com>
2023-03-26 08:57:27 +0200

    qlc

--- WebApplication2/Controllers/OrginizationController.cs
+++ WebApplication2/Controllers/OrginizationController.cs
@@ -68,11 +68,11 @@ namespace WebApplication2.Controllers
         {
             var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
             var orginization = _mapper.Map<Orginization>(orginizationCreateDto);
-            
+
             _repo.addOrgenization(orginization);
             UserOrg userOrg = new()
             {
-                OrginizationId= orginization.Id,
+                OrginizationId = orginization.Id,
                 UserId = userId,
                 Role = OrgRole.Creator
             };
@@ -119,12 +119,12 @@ namespace WebApplication2.Controllers
 
 
 
+        [Route("users")]
         [Authorize]
-        [Route("Users")]
         [HttpPost]
         public IActionResult AddMembers(UserOrgCreateDto userOrgCreateDto)
         {
-            if(userOrgCreateDto.role==OrgRole.Creator)
+            if (userOrgCreateDto.role == OrgRole.Creator)
             {
                 return Forbid();
             }
@@ -132,26 +132,27 @@ namespace WebApplication2.Controllers
             _repo.AddOrgUser(userOrg);
             return NoContent();
         }
-        //[Route("Users")]
+
+        
         [Authorize]
-        [HttpPatch("{id}")]
-        public IActionResult ChangeRole(int id,string userId,JsonPatchDocument<UserOrgCreateDto> patchDoc)
+        [HttpPatch("users/{id}")]
+        public IActionResult ChangeRole(int id, string userId, JsonPatchDocument<UserOrgCreateDto> patchDoc)
         {
             //getting the id of the user who made the request
             var RUId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
 
             var RUOrg = _repo.GetOrgUser(id, RUId);
-            var userOrgFromRepo = _repo.GetOrgUser(id,userId);
+            var userOrgFromRepo = _repo.GetOrgUser(id, userId);
             //checking if the requesting user has authority for the request
             if (userOrgFromRepo == null)
             {
                 return NotFound();
             }
-            if (RUOrg == null || RUOrg.Role<userOrgFromRepo.Role)
+            if (RUOrg == null || RUOrg.Role < userOrgFromRepo.Role)
             {
                 return Unauthorized();
             }
-            
+
             var userOrgToPatch = _mapper.Map<UserOrgCreateDto>(userOrgFromRepo);
 
             patchDoc.ApplyTo(userOrgToPatch, ModelState);
@@ -160,33 +161,34 @@ namespace WebApplication2.Controllers
                 return ValidationProblem(ModelState);
             }
             //temporary???
-            if(userOrgToPatch.role==OrgRole.Admin)
+            if (userOrgToPatch.role == OrgRole.Admin)
             {
                 return Forbid();
             }
-            
+
             _mapper.Map(userOrgToPatch, userOrgFromRepo);
-            
+
             _repo.UpdateRole(userOrgFromRepo);
             return NoContent();
         }
-        //[Route("Users")]
-        //[Authorize]
-        //[HttpDelete("{id}")]
-        //public IActionResult DeleteUserOrg(int id, string userId)
-        //{
+        
+        [Authorize]
+        [HttpDelete("users/{id}")]
+        public IActionResult DeleteUserOrg(int id, string userId)
+        {
 
-        //    var RUId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
-        //    var RUOrg = _repo.GetOrgUser(id, RUId);
-        //    var userOrgFromRepo = _repo.GetOrgUser(id, userId);
-        //    if (RUOrg == null || RUOrg.Role < userOrgFromRepo.Role)
-        //    {
-        //        return Unauthorized();
-        //    }
+            var RUId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var RUOrg = _repo.GetOrgUser(id, RUId);
+            var userOrgFromRepo = _repo.GetOrgUser(id, userId);
+            if (RUOrg == null || RUOrg.Role < userOrgFromRepo.Role)
+            {
+                return Unauthorized();
+            }
 
-        //    _repo.DeleteOrgUser(userOrgFromRepo);
-        //    return NoContent();
-        //}
+            _repo.DeleteOrgUser(userOrgFromRepo);
+            return NoContent();
+        }
+
     }
 }
 
